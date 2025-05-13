8fa6fbb70833593b967427587e2b7d28da634e2e
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-05-13 22:08:00 +0200

    Add logic for adding and rendering lists

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './styles/main.css';
+import { showListModal, confirmListAdd, cancelListAdd } from './components/addList.js';
+import { renderList } from './components/renderLists.js';
+
+const lists = [];
+
+const listModalBtn = document.querySelector('#addListModal');
+const listConfirm = document.querySelector('#list-confirm');
+const listCancel = document.querySelector('#list-cancel');
+const listContainer = document.querySelector('#lists-container');
+
+listModalBtn.addEventListener('click', showListModal);
+listConfirm.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    let newList = confirmListAdd();
+
+    lists.push(newList);
+
+    // WIP, CHANGE LATER TO A SINGLE FUNCTION FOR DRY
+    listContainer.innerHTML = '';
+    lists.forEach((list) => {
+        let rendered = renderList(list.name);
+        console.log(list.name);
+        console.log(typeof list.name);
+
+        listContainer.appendChild(rendered);
+        console.log(list.name);
+    });
+    
+});
+
+listCancel.addEventListener('click', cancelListAdd);
