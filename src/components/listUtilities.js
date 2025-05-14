b19d16b0d87ed1ef1116e1fe158f81941247a769
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-05-14 19:51:08 +0200

    Move renderList into listUtilities

--- src/components/listUtilities.js
+++ src/components/listUtilities.js
@@ -1,10 +1,8 @@
 import { createList } from "./objects.js";
+import edit from '../assets/pencil.png';
+import del from '../assets/delete.png';
 
 const listModal = document.querySelector('#listDialog');
-
-export function showListModal() {
-    listModal.showModal();
-}
 
 export function confirmListAdd() {
     const name = document.querySelector('#list-name');
@@ -20,10 +18,38 @@ export function cancelListAdd(event) {
     listModal.close();
 }
 
+export function renderList(name) {
+    const listDiv = document.createElement('div');
+    listDiv.classList.add('list-item');
+
+    const listName = document.createElement('h2');;
+    listName.textContent = name;
+
+    const iconsDiv = document.createElement('div');
+    iconsDiv.classList.add('list-icons');
+
+    const editIcon = document.createElement('img');
+    editIcon.src = edit;
+    editIcon.classList.add('icon');
+
+    const deleteIcon = document.createElement('img');
+    deleteIcon.src = del;
+    deleteIcon.classList.add('icon');
+
+    iconsDiv.append(editIcon, deleteIcon);
+    listDiv.append(listName, iconsDiv);
+
+    return listDiv;
+}
+
+export function showListModal() {
+    listModal.showModal();
+}
+
 export function deleteList() {
 
 }
 
 export function editList() {
-    
+
 }
