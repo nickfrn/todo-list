cc0f736675255d719b6814ee56ec5ad66f42895c
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-05-14 19:47:21 +0200

    Remove testing logs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles/main.css';
-import { showListModal, confirmListAdd, cancelListAdd } from './components/addList.js';
+import { showListModal, confirmListAdd, cancelListAdd } from './components/listUtilities.js';
 import { renderList } from './components/renderLists.js';
 
 const lists = [];
@@ -17,17 +17,11 @@ listConfirm.addEventListener('click', (event) => {
 
     lists.push(newList);
 
-    // WIP, CHANGE LATER TO A SINGLE FUNCTION FOR DRY
     listContainer.innerHTML = '';
     lists.forEach((list) => {
         let rendered = renderList(list.name);
-        console.log(list.name);
-        console.log(typeof list.name);
-
         listContainer.appendChild(rendered);
-        console.log(list.name);
     });
-    
 });
 
 listCancel.addEventListener('click', cancelListAdd);
