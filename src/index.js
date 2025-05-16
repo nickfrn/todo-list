6392b2d00642c9694a3ef4a753db9ebc3abe75eb
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-05-16 15:02:47 +0200

    Move lists handling logic in utilities

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import './styles/main.css';
 import { showListModal, confirmListAdd, cancelListAdd, renderList, editList, deleteList } from './components/listUtilities.js';
 
-const lists = [];
-
 const listModalBtn = document.querySelector('#addListModal');
 const listConfirm = document.querySelector('#list-confirm');
 const listCancel = document.querySelector('#list-cancel');
-const listContainer = document.querySelector('#lists-container');
 
 listModalBtn.addEventListener('click', showListModal);
 listConfirm.addEventListener('click', (event) => {
     event.preventDefault();
-    
-    let newList = confirmListAdd();
-
-    lists.push(newList);
-
-    listContainer.innerHTML = '';
-    lists.forEach((list) => {
-        let rendered = renderList(list);
-        listContainer.appendChild(rendered);
-    });
+    confirmListAdd();
 });
 
 listCancel.addEventListener('click', cancelListAdd);
