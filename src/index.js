325a9d914b82e3b3909974d018d3a0791d4b3309
nickfrn <193815257+nickfrn@users.noreply.github.com>
2026-07-13 14:49:28 +0200

    Update index, fix typo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const listModalBtn = document.querySelector('#add-list-modal');
 const listConfirm = document.querySelector('#create-list-confirm');
 const listCancel = document.querySelector('#create-list-cancel');
 const editListConf = document.querySelector('#edit-list-confirm');
-const editListCanc = document.querySelector('edit-list-cancel');
+const editListCanc = document.querySelector('#edit-list-cancel');
 
 listModalBtn.addEventListener('click', showListModal);
 listConfirm.addEventListener('click', (event) => {
