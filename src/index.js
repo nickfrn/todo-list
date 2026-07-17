89d0656d969b1cce40c3b5c9ec4fe7c6db4426d8
nickfrn <193815257+nickfrn@users.noreply.github.com>
2026-07-17 17:30:37 +0200

    Add listener to 'add task' button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './styles/main.css';
 import { initLists, showListModal, confirmListAdd, modalCancelBtn, deleteList, confirmEditList } from './components/listUtilities.js';
+import { showTaskModal } from './components/tasksUtilities.js';
 
 const listModalBtn = document.querySelector('#add-list-modal');
 const listConfirm = document.querySelector('#create-list-confirm');
 const listCancel = document.querySelector('#create-list-cancel');
 const editListConf = document.querySelector('#edit-list-confirm');
 const editListCanc = document.querySelector('#edit-list-cancel');
+const taskModalBtn = document.querySelector('#add-task-btn');
 
 // listener to display modals for adding or editing lists
 listModalBtn.addEventListener('click', showListModal);
@@ -23,5 +25,8 @@ editListConf.addEventListener('click', (event) => {
     confirmEditList(event);
 });
 editListCanc.addEventListener('click', modalCancelBtn);
+
+// Listener to disply modals for adding or editing tasks
+taskModalBtn.addEventListener('click', showTaskModal);
 
 initLists();
