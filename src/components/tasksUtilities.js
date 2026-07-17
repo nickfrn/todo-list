e7cc67b6ce2871b2a64ee9c639313d0ef0f460ac
nickfrn <193815257+nickfrn@users.noreply.github.com>
2026-07-17 18:32:57 +0200

    Add refreshTasksUI

--- src/components/tasksUtilities.js
+++ src/components/tasksUtilities.js
@@ -1,9 +1,29 @@
 import { createTask } from "./objects";
 
+let tasks = [];
+
 const taskModal = document.querySelector('#createTaskDialog');
+const tasksContainer = document.querySelector('#tasks-container');
 
 export function showTaskModal(event) {
     if (event.target.id === 'add-task-btn') {
         taskModal.showModal();
     }
 }
+
+// Inner function to refresh the UI when a change is made to the tasks
+function refreshTasksUI() {
+    tasksContainer.innerHTML = '';
+    tasks.forEach((task) => {
+        let rendered = renderTask(task);
+        tasksContainer.appendChild(rendered);
+    });
+}
+
+ export function confirmTaskAdd() {
+
+ }
+// cancelModalBtn()
+// renderTasks()
+// deleteTask()
+// confirmEditTask()
