e4ff835aaf8dd089959248fee1a27866d08c162b
nickfrn <193815257+nickfrn@users.noreply.github.com>
2026-07-17 11:51:18 +0200

    Add default list initialization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles/main.css';
-import { showListModal, confirmListAdd, cancelListAdd, deleteList, confirmEditList } from './components/listUtilities.js';
+import { initLists, showListModal, confirmListAdd, cancelListAdd, deleteList, confirmEditList } from './components/listUtilities.js';
 
 const listModalBtn = document.querySelector('#add-list-modal');
 const listConfirm = document.querySelector('#create-list-confirm');
@@ -7,15 +7,19 @@ const listCancel = document.querySelector('#create-list-cancel');
 const editListConf = document.querySelector('#edit-list-confirm');
 const editListCanc = document.querySelector('#edit-list-cancel');
 
+// listener to display modals for adding or editing lists
 listModalBtn.addEventListener('click', showListModal);
+
+// Confirm and cancel listeners on list creation modal
 listConfirm.addEventListener('click', (event) => {
     event.preventDefault();
     confirmListAdd();
 });
-
 listCancel.addEventListener('click', cancelListAdd);
 
 editListConf.addEventListener('click', (event) => {
     event.preventDefault();
     confirmEditList(event);
 });
+
+initLists();
