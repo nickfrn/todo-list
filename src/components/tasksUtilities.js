35a993c90dc8844870b6ffa32866344a13f260a8
nickfrn <193815257+nickfrn@users.noreply.github.com>
2026-07-17 17:30:15 +0200

    Add showTaskModal function

--- src/components/tasksUtilities.js
+++ src/components/tasksUtilities.js
@@ -1,1 +1,9 @@
 import { createTask } from "./objects";
+
+const taskModal = document.querySelector('#createTaskDialog');
+
+export function showTaskModal(event) {
+    if (event.target.id === 'add-task-btn') {
+        taskModal.showModal();
+    }
+}
