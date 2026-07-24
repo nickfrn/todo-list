e3511d0b8ebd5b00886ebcfcfa06005b6c68acfc
nickfrn <193815257+nickfrn@users.noreply.github.com>
2026-07-24 15:23:14 +0200

    Update showTaskModal and renderTask

--- src/components/tasksUtilities.js
+++ src/components/tasksUtilities.js
@@ -1,8 +1,11 @@
 import { createTask } from "./objects";
+import edit from '../assets/pencil.png';
+import del from '../assets/delete.png';
 
 let tasks = [];
 
 const taskModal = document.querySelector('#createTaskDialog');
+const editTaskModal = document.querySelector('#editTaskDialog');
 const tasksContainer = document.querySelector('#tasks-container');
 
 // Inner function to refresh the UI when a change is made to the tasks
@@ -17,7 +20,20 @@ function refreshTasksUI() {
 export function showTaskModal(event) {
     if (event.target.id === 'add-task-btn') {
         taskModal.showModal();
-    }
+    } else if (event.target.getAttribute('data-type') === 'edit-task') {
+        // Find ID of the task item
+        const taskItem = event.target.closest('.task-item');
+        const taskId = listItem.getAttribute('data-id');
+
+        // Save ID inside the modal element
+        editTaskModal.setAttribute('data-editing-id', taskId);
+
+        // Pre-fill input box with old name
+        const taskToEdit = tasks.find(task => task.id === taskId);
+        document.querySelector('#new-task-name').value = taskToEdit.name;
+
+        editTaskModal.showModal();
+    } 
 }
 
  export function confirmTaskAdd() {
@@ -66,6 +82,26 @@ function renderTask(task) {
     taskDate.classList.add('task-date');
 
     taskItem.append(taskTitle, taskDesc, taskPriority, taskDate);
+
+    // Add edit and delete icons to the task item
+    const iconsDiv = document.createElement('div');
+    iconsDiv.classList.add('list-icons');
+
+    const editIcon = document.createElement('img');
+    editIcon.src = edit;
+    editIcon.classList.add('icon');
+    editIcon.classList.add('edit');
+    editIcon.setAttribute('data-type', 'edit-task');
+    editIcon.addEventListener('click', showTaskModal);
+
+    const deleteIcon = document.createElement('img');
+    deleteIcon.src = del;
+    deleteIcon.classList.add('icon');
+    deleteIcon.classList.add('delete');
+    deleteIcon.addEventListener('click', deleteTask);
+
+    iconsDiv.append(editIcon, deleteIcon);
+    taskItem.append(listName, iconsDiv);
 
     return taskItem;
 }
