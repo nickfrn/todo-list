8ccd463b79ac63a7c14f752f1e4a5542a906d75b
nickfrn <193815257+nickfrn@users.noreply.github.com>
2026-07-24 11:04:50 +0200

    Update confirmTaskAdd

--- src/components/tasksUtilities.js
+++ src/components/tasksUtilities.js
@@ -5,12 +5,6 @@ let tasks = [];
 const taskModal = document.querySelector('#createTaskDialog');
 const tasksContainer = document.querySelector('#tasks-container');
 
-export function showTaskModal(event) {
-    if (event.target.id === 'add-task-btn') {
-        taskModal.showModal();
-    }
-}
-
 // Inner function to refresh the UI when a change is made to the tasks
 function refreshTasksUI() {
     tasksContainer.innerHTML = '';
@@ -20,17 +14,30 @@ function refreshTasksUI() {
     });
 }
 
-// TODO: Continue from here
+export function showTaskModal(event) {
+    if (event.target.id === 'add-task-btn') {
+        taskModal.showModal();
+    }
+}
+
  export function confirmTaskAdd() {
     const title = document.querySelector('#task-title');
     const desc = document.querySelector('#task-desk');
     const priority = document.querySelector('#task-priority');
     const dueDate = document.querySelector('#task-date');
 
-    // TODO: Finish logic
+    let task = createTask(title, desc, priority, dueDate);
+    tasks.push(task);
+
+    // Clean modal parameters and close it
+    title.value = '';
+    desc.value = '';
+    taskModal.close();
+
+    refreshTasksUI();
  }
 
- // TODO
+// TODO
 // cancelModalBtn()
 // renderTasks()
 // deleteTask()
